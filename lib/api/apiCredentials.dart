00ab47c0fd26d6284d3e5873e0729ec1baef2adb
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-12-15 22:06:00 +0530

    API Creds added

---
+++ lib/api/apiCredentials.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_ml_vision_del/utils/credsConstants.dart';
+
+class APICreds {
+  static String api_id = CredsConstants.api_id;
+  static String api_key = CredsConstants.api_key;
+}
