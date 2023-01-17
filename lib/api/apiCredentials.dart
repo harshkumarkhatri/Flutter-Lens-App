4ecef48975db149686a7c79eba44b689f0a5acd2
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2023-01-17 23:38:26 +0530

    Final commit

--- lib/api/apiCredentials.dart
+++ lib/api/apiCredentials.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_ml_vision_del/utils/credsConstants.dart';
+// import 'package:flutter_ml_vision_del/utils/credsConstants.dart';
 
 class APICreds {
-  static String api_id = CredsConstants.api_id;
-  static String api_key = CredsConstants.api_key;
+  static String api_id = "2472b94a";
+  static String api_key = "0b35f61b485bec92dd49eeb7f58af168";
 }
